c5e865d6ac9674204f885f6bfa76d52011721693
fernandouval <fernandouval@gmail.com>
2025-06-10 16:14:19 -0300

    Ajustes de estilo y textos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,22 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  # The path used after sign up.
+  def after_sign_in_path_for(resource)
+    if resource.class.name == "Company"
+      if resource.is_main_company
+        dashboard_path
+      else
+        if resource.evaluations.length > 0
+          dashboard_path
+        else
+          company_answer_poll_path
+        end
+      end
+    else
+      super(resource)
+    end
+  end
+  #
 end
